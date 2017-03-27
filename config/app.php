b41ee1744a2d6db91314c00f1b3e6b08b5ee455b
Artem Zinoviev <arzinoviev@gmail.com>
2017-03-27 16:03:17 +0300

    base integration of laravel-backpack

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Backpack\Base\BaseServiceProvider::class,
+        Backpack\CRUD\CrudServiceProvider::class,
+
     ],
 
     /*
